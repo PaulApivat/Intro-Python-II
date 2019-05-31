626e90be963430f5d351d96548e666f3ef3dd189
paulapivat <paul.apivat@gmail.com>
2019-05-31 13:49:23 +0700

    initialize Room class in room.py, print room in adv.py

--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    
+    def __init__(self, name, description):
+        self.name = name 
+        self.description = description
+
+    def __repr__(self):
+        return "The current room is.. " + self.name + "..description " + self.description
+
+my_room = Room("Placeholder Room", "Foyer")
+print(my_room)
